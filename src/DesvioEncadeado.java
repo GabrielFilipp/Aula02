aacc67a5b6fc051300546d2916cc2f94f6a15040
Gabriel Filipp <filipp278@gmail.com>
2021-06-25 19:47:00 -0300

    Implementados os desvios comdicionais

--- src/DesvioEncadeado.java
+++ src/DesvioEncadeado.java
@@ -3,19 +3,31 @@ import java.util.Scanner;
 public class DesvioEncadeado {
 
 	public static void main(String[] args) {
-		/*Uma ONG resolveu prestar um serviço bem diferente: ela oferece vans para buscar pessoas com qualquer tipo de dificuldade de locomoção para poderem votar.
-		 * Para evitar problemas no momento do embarque, porém, você foi convidado a criar um programa que valide a idade dos passageiros: caso tenham 16 anos ou menos, não podem votar (e nem embarcar). Caso tenham entre 16 anos e 18 incompletos, podem optar por votar ou não. Caso tenham mais de 18 anos, devem votar obrigatoriamente.
-		 * Crie um programa que receba a idade dos passageiros em potencial e indique se podem embarcar e votar.*/
-		
+		/*
+		 * Uma ONG resolveu prestar um serviço bem diferente: ela oferece vans para
+		 * buscar pessoas com qualquer tipo de dificuldade de locomoção para poderem
+		 * votar. Para evitar problemas no momento do embarque, porém, você foi
+		 * convidado a criar um programa que valide a idade dos passageiros: caso tenham
+		 * 16 anos ou menos, não podem votar (e nem embarcar). Caso tenham entre 16 anos
+		 * e 18 incompletos, podem optar por votar ou não. Caso tenham mais de 18 anos,
+		 * devem votar obrigatoriamente. Crie um programa que receba a idade dos
+		 * passageiros em potencial e indique se podem embarcar e votar.
+		 */
+
 		Scanner leitor = new Scanner(System.in);
 		int idade;
 		System.out.println("Por favor, digite a idade do passageiro.");
 		idade = leitor.nextInt();
-	
-		/*Aqui devemos criar uma solução para realizar as verificações.*/
-		
-		
-	
+
+		/* Aqui devemos criar uma solução para realizar as verificações. */
+		if (idade < 16) {
+			System.out.println("Você não pode votar (nem embarcar)!");
+		} else if (idade >= 18) {
+			System.out.println("Você deve votar obrigatoriamente!");
+		} else {
+			System.out.println("Você pode optar por votar ou não!");
+		}
+
 		leitor.close();
 
 	}
